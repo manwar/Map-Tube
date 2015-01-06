90190970e11dede0e10789ee5fe7381bafaf5d91
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-06 17:39:53 +0000

    - Oops forgot to update Changes file.

--- lib/Map/Tube/Error.pm
+++ lib/Map/Tube/Error.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Error;
 
-$Map::Tube::Error::VERSION   = '2.59';
+$Map::Tube::Error::VERSION   = '2.60';
 $Map::Tube::Error::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::Error - Error class for the library Map::Tube.
 
 =head1 VERSION
 
-Version 2.59
+Version 2.60
 
 =cut
 
