9bf15a6846775c70a82e07201c3f08f43ca6cb6e
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2016-07-03 03:24:16 +0100

    - Addressed issue raised in the CPAN Tester Report.   http://www.cpantesters.org/cpan/report/297cf692-3f1d-11e6-a514-8183992ad5f0

--- lib/Map/Tube.pm
+++ lib/Map/Tube.pm
@@ -1,6 +1,6 @@
 package Map::Tube;
 
-$Map::Tube::VERSION   = '3.15';
+$Map::Tube::VERSION   = '3.16';
 $Map::Tube::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube - Core library as Role (Moo) to process map data.
 
 =head1 VERSION
 
-Version 3.15
+Version 3.16
 
 =cut
 
