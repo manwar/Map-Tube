4f5545bfc3c4e081b979203962028c6c2c61b5a7
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-02-19 07:24:42 +0000

    - Added dependency on MooX::Options v4.023. - Added dependency on Map::Tube::Exceptions v3.11. - Refactored script map-data-converter to handle error appropriately.

--- lib/Map/Tube/Utils.pm
+++ lib/Map/Tube/Utils.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Utils;
 
-$Map::Tube::Utils::VERSION   = '3.23';
+$Map::Tube::Utils::VERSION   = '3.24';
 $Map::Tube::Utils::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::Utils - Helper package for Map::Tube.
 
 =head1 VERSION
 
-Version 3.23
+Version 3.24
 
 =cut
 
