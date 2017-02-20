90156a54e8eea9c67beb631ca7fdaecb31503ca6
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2017-02-20 11:54:13 +0000

    - Removed dependency on XML::Simple. - Refactored script map-data-converter to workout with XML::Twig when creating xml file.

--- lib/Map/Tube/Route.pm
+++ lib/Map/Tube/Route.pm
@@ -1,6 +1,6 @@
 package Map::Tube::Route;
 
-$Map::Tube::Route::VERSION   = '3.24';
+$Map::Tube::Route::VERSION   = '3.25';
 $Map::Tube::Route::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube::Route - Class to represent the route in the map.
 
 =head1 VERSION
 
-Version 3.24
+Version 3.25
 
 =cut
 
