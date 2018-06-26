8429727a32c2f1ca459d846a13a03733ac01ae0c
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2018-06-26 11:00:46 +0100

    - Used File::ShareDir v1.114 instead of File::Share to cover the FAIL report.   http://www.cpantesters.org/cpan/report/548008f4-7312-11e8-9697-652b7347484a - Upgraded min version File::ShareDir::Install v0.13.

--- lib/Map/Tube.pm
+++ lib/Map/Tube.pm
@@ -1,6 +1,6 @@
 package Map::Tube;
 
-$Map::Tube::VERSION   = '3.51';
+$Map::Tube::VERSION   = '3.52';
 $Map::Tube::AUTHORITY = 'cpan:MANWAR';
 
 =head1 NAME
@@ -9,7 +9,7 @@ Map::Tube - Lightweight Routing Framework.
 
 =head1 VERSION
 
-Version 3.51
+Version 3.52
 
 =cut
 
